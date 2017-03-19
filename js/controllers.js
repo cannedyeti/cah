a361b819596c0d35aab333a0bce7f80287bba1ff
CannedYeti <cpotebnya@gmail.com>
2017-03-19 16:15:04 -0700

    cought up to previous build. Added color change on sleceted card and displaying answers

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,18 @@
 angular.module('CardsAgainstAssembly')
-.controller('CardsCtrl', ['$scope', "QuestionsFactory", "AnswersFactory", function($scope, QuestionsFactory, AnswersFactory) {
+.controller('CardsCtrl', ['$scope', "QuestionsFactory", "AnswersFactory", "sharedProperties", function($scope, QuestionsFactory, AnswersFactory, sharedProperties) {
   $scope.qCards = QuestionsFactory.getCards();
   $scope.displayCard = $scope.qCards[pickCardIndex($scope.qCards.length)];
-  $scope.numPlayers = 3;
   $scope.errorMessage = "";
+  var CARDS_PER_PLAYER = 2;
+  $scope.playerCards = [];
+  $scope.selectedAnswers = [];
+
+  $scope.numPlayers = sharedProperties.getNumPlayers();
+
+  for (i = 0; i < $scope.numPlayers; i++) {
+        $scope.playerCards.push(shuffleArray(AnswersFactory.getCards(), CARDS_PER_PLAYER))
+      };
+      
 
   $scope.$watch("numPlayers", function(newVal, oldVal){
     $scope.errorMessage = "";
@@ -13,8 +22,36 @@ angular.module('CardsAgainstAssembly')
       $scope.errorMessage = "Too many players";
     }
   });
+  $scope.assignAnswers = function() {
+    sharedProperties.setNumPlayers($scope.numPlayers);
+  }
+  function shuffleArray(arr, limit) {
+    if(limit > arr.length) {
+      limit = arr.length;
+    }
+    var shuffled = arr.sort(function() {
+      return 0.5 - Math.random();
+    });
+    return shuffled.slice(0, limit);
+  }
 
-}]);
+  $scope.selectAnswers = function(playerIndex, card) {
+    $scope.selectedAnswers[playerIndex] = card;
+  }
+
+}])
+.service("sharedProperties", function(){
+  var numPlayers = 3;
+
+  return {
+    getNumPlayers: function() {
+      return numPlayers;
+    },
+    setNumPlayers: function(value) {
+      numPlayers = value;
+    }
+  }
+});
 
 function pickCardIndex(size){
   return Math.floor(Math.random() * size);
